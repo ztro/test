7c360464f62058e028b3d04ccb0a8af52adcf565
Dwie Ztro <dwie.stro@gmail.com>
2018-06-04 12:42:43 +0700

    comit kedua

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 error_reporting(0);
 session_start();
 include('database_connection.php');include('function.php');
@@ -7,4 +8,5 @@ include('header.php');
 ?>
 	<span id="alert_action"></span>
 <?php include "module.php";include("footer.php");
+//test aja ya..
 ?>
